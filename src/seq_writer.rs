66a81ef6ea1a3d2bbc7169b29b06a438966b63f2
Andrey <amigin@gmail.com>
2021-11-29 23:23:44 +0200

    Made Ability to parse Seq Connection String

--- src/seq_writer.rs
+++ src/seq_writer.rs
@@ -10,14 +10,65 @@ pub struct SeqWriter {
     pub app: String,
 }
 
+const DEFAULT_FLUSH_SLEEP: u64 = 1;
+const DEFAULT_FLUSH_CHUNK: usize = 50;
+
 impl SeqWriter {
     pub fn new(url: String, api_key: Option<String>, app: String) -> Self {
         Self {
             url,
             api_key,
-            max_logs_flush_chunk: 10,
-            flush_delay: Duration::from_secs(1),
+            max_logs_flush_chunk: DEFAULT_FLUSH_CHUNK,
+            flush_delay: Duration::from_secs(DEFAULT_FLUSH_SLEEP),
             app,
+        }
+    }
+
+    pub fn fron_connection_string(connection_string: String, app: String) -> Self {
+        if connection_string.to_lowercase().starts_with("http") {
+            return Self::new(connection_string, None, app);
+        }
+
+        let mut url = None;
+        let mut api_key = None;
+        let mut max_logs_flush_chunk = DEFAULT_FLUSH_CHUNK;
+        let mut flush_delay = DEFAULT_FLUSH_SLEEP;
+
+        for item in connection_string.split(';') {
+            let (key, value) = spit_key_value(item);
+
+            match key {
+                "Url" => {
+                    url = Some(value);
+                }
+                "ApiKey" => {
+                    api_key = Some(value.to_string());
+                }
+                "FlushLogsChunk" => {
+                    max_logs_flush_chunk = value
+                        .parse::<usize>()
+                        .expect("FlushLogsChunk must be a number");
+                }
+                "FlushDelay" => {
+                    let value = value.parse::<usize>().expect("FlushDelay must be a number");
+                    flush_delay = value as u64;
+                }
+                _ => {
+                    panic!("Invalid key {} of seq connection string ", key);
+                }
+            }
+        }
+
+        if url.is_none() {
+            panic!("There is no URL parameter in seq connection string");
+        }
+
+        Self {
+            url: url.unwrap().to_string(),
+            api_key,
+            app,
+            flush_delay: Duration::from_secs(flush_delay),
+            max_logs_flush_chunk,
         }
     }
 
@@ -94,4 +145,41 @@ async fn flush_events(
     }
 
     result
+}
+
+fn spit_key_value(str: &str) -> (&str, &str) {
+    let index = str.find('=');
+
+    if index.is_none() {
+        panic!("Invalid {} key value of seq connection string", str);
+    }
+
+    let index = index.unwrap();
+
+    return (&str[..index], &str[index + 1..]);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::spit_key_value;
+
+    #[test]
+    fn test_split_key_value() {
+        let str = "A=B";
+
+        let (key, value) = spit_key_value(str);
+
+        assert_eq!("A", key);
+        assert_eq!("B", value);
+    }
+
+    #[test]
+    fn test_split_key_value_empty_value() {
+        let str = "A=";
+
+        let (key, value) = spit_key_value(str);
+
+        assert_eq!("A", key);
+        assert_eq!("", value);
+    }
 }
